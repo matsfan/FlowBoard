05f343be7044c37e294e4c0205d976bb2b9ca059
Mike Price <idflyfish@hotmail.com>
2025-08-20 16:46:50 -0600

    project rename

--- src/archived_FlowBoard.Web/Endpoints/Boards/CreateBoardRequest.cs
+++
@@ -1,6 +0,0 @@
-namespace FlowBoard.Web.Endpoints.Boards;
-
-public sealed class CreateBoardRequest
-{
-    public string Name { get; set; } = string.Empty;
-}
--- src/archived_FlowBoard.Web/Endpoints/Boards/CreateBoardResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace FlowBoard.Web.Endpoints.Boards;
-
-public sealed class CreateBoardResponse
-{
-    public Guid Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public DateTimeOffset CreatedUtc { get; set; }
-}
