819bfaefb2d83c96c77e387364bc8d1e77526483
Mike Price <idflyfish@hotmail.com>
2025-09-20 11:49:40 -0600

    made descriptions nullable

--- src/FlowBoard.WebApi/Endpoints/Cards/GetById/GetCardByIdResponse.cs
+++ src/FlowBoard.WebApi/Endpoints/Cards/GetById/GetCardByIdResponse.cs
@@ -1,4 +1,4 @@
 
 namespace FlowBoard.WebApi.Endpoints.Cards.Get;
 
-public sealed class GetCardResponse { public Guid Id { get; set; } public Guid BoardId { get; set; } public Guid ColumnId { get; set; } public string Title { get; set; } = string.Empty; public string Description { get; set; } = string.Empty; public int Order { get; set; } public bool IsArchived { get; set; } public DateTimeOffset CreatedUtc { get; set; } }
+public sealed class GetCardResponse { public Guid Id { get; set; } public Guid BoardId { get; set; } public Guid ColumnId { get; set; } public string Title { get; set; } = string.Empty; public string? Description { get; set; } = string.Empty; public int Order { get; set; } public bool IsArchived { get; set; } public DateTimeOffset CreatedUtc { get; set; } }
